b26761678d1b9f4597710d9baa15e20d3b569605
Jonathan Xu <jonathan@randomguava.com>
2014-01-31 22:02:06 -0800

    first commit

---
+++ CP.TwitterClient/CPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CPAppDelegate.h
+//  CP.TwitterClient
+//
+//  Created by Jonathan Xu on 1/31/14.
+//  Copyright (c) 2014 Jonathan Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CP.TwitterClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CP.TwitterClient
+//
+//  Created by Jonathan Xu on 1/31/14.
+//  Copyright (c) 2014 Jonathan Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CPAppDelegate class]));
+    }
+}
